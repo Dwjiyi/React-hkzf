b488d6fec3939ec42e559da151c60499f5bd270e
wangyu <wangyu@licaigc.com>
2020-10-30 19:33:42 +0800

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Button } from 'antd'
+
+// const { Button } = require("antd");
+
+function App() {
+  return (
+    <div className="App">
+ 
+      <Button>我是按钮</Button>
+
+    </div>
+  );
+}
+
+export default App;
